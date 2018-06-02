c7d2f11461c2cf36c62eb4dad1c8cbe408beaf3f
jStojcheski <jugoslav.stojcheski@gmail.com>
2018-06-02 16:43:34 +0000

    Add run.py (final version for Raspberry Pi)

---
+++ pyimagesearch/run.py
@@ -0,0 +1,93 @@
+# https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
+
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import cv2
+import time
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=16, help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the ball in the HSV color
+# space, then initialize the list of tracked points
+greenLower = (0, 120, 60)
+greenUpper = (35, 255, 255)
+pts = deque(maxlen=args["buffer"])
+
+# initialize the video stream and allow the camera sensor to warmup
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+
+# keep looping
+while True:
+    # grab the frame from the threaded video stream
+    frame = vs.read()
+    
+    # resize the frame, blur it, and convert it to the HSV color space
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    frame = imutils.resize(frame, width=600)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # construct a mask for the color "green", then perform a series of dilations
+    # and erosions to remove any small blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use it to compute the
+        # minimum enclosing circle and centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                       (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            # print (x, y, radius)
+            print('x: {:6.2f} | y: {:6.2f} | r: {:6.2f}'.format(x, y, radius))
+    
+    # update the points queue
+    pts.appendleft(center)
+
+    # loop over the set of tracked points
+    for i in range(1, len(pts)):
+        # if either of the tracked points are None, ignore them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+    
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    # show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+
+# cleanup the camera and close any open windows
+cv2.destroyAllWindows()
