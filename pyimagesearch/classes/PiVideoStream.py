e70690d3fa5b82c834c50b6a5efaa2013997e742
Jugoslav Stojcheski <jugoslav.stojcheski@gmail.com>
2018-06-02 17:46:46 +0200

    Update: horizontal & vertical flip feature @ PiVideoStream.py (test)

--- pyimagesearch/classes/PiVideoStream.py
+++ pyimagesearch/classes/PiVideoStream.py
@@ -6,22 +6,22 @@ from picamera import PiCamera
 from threading import Thread
 import cv2
 
+
 class PiVideoStream:
     def __init__(self, resolution=(320, 240), framerate=32):
         # initialize the camera and stream
         self.camera = PiCamera()
-
-        # flip the view, if necessary
-        self.camera.hflip = True  # horizontal flip
-        self.camera.vflip = True  # vertical flip
-
         self.camera.resolution = resolution
         self.camera.framerate = framerate
         self.rawCapture = PiRGBArray(self.camera, size=resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture,
                                                      format="bgr",
                                                      use_video_port=True)
-        
+
+        # flip the view, if necessary
+        self.camera.hflip = True
+        self.camera.vflip = True
+
         # initialize the frame and the variable used to indicate
         # if the thread should be stopped
         self.frame = None
--- pyimagesearch/picamera_video_test.py
+++ pyimagesearch/picamera_video_test.py
@@ -12,10 +12,6 @@ camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
-# flip the view, if necessary
-# camera.hflip = True  # horizontal flip
-# camera.vflip = True  # vertical flip
-
 # allow the camera to warmup
 time.sleep(0.1)
 
